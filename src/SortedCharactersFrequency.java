d79b07d77608e489556654bd79e4891d28d5541c
klaus19 <tejaskhartude@gmail.com>
2022-12-12 21:31:39 +0530

    updates added

---
+++ src/SortedCharactersFrequency.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SortedCharactersFrequency {
+
+    public int frequencySort(int [][]A) {
+
+        int n=A.length;
+        int m = A[0].length;
+        int dp[][] = new int[n][m];
+
+        dp[0][0]=A[0][0];
+        dp[0][1]=A[0][1];
+        dp[0][2]=A[0][2];
+        for(int i=1;i<n;i++){
+            dp[i][0]=A[i][0]+Math.min(dp[i-1][1],dp[i-1][2]);
+            dp[i][1]=A[i][1]+Math.min(dp[i-1][0],dp[i-1][2]);
+            dp[i][2]=A[i][2]+Math.min(dp[i-1][1],dp[i-1][0]);
+        }
+        return Math.min(dp[n-1][0],Math.min(dp[n-1][1],dp[n-1][2]));
+
+        }
+
+
+    public static void main(String[] args) {
+        SortedCharactersFrequency sp = new SortedCharactersFrequency();
+        int[][]arr = new int[][]{{2,9,4},{20,7,15},{18,12,19}};
+        System.out.println(sp.frequencySort(arr));
+    }
+}
---
+++ src/SumDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumDigits {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int sum = 0;
+            while (a>0){
+                sum+=a%10;
+                a= (int) Math.floor(a / 10);
+                System.out.println(sum);
+            }
+        }
+    }
+}
